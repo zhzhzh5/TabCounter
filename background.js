0d64dd7c71fb598eca012145426ec51a7d3c6509
Jun209 <junfeiyugu@gmail.com>
2021-05-24 16:05:39 -0500

    Update background.js

--- background.js
+++ background.js
@@ -3,6 +3,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendReponse) {
   window.tabs.push(request.url);
 });
 
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  window.tabs.push(tab.url);
+});
+
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.create({ url: "popup.html" });
 });
